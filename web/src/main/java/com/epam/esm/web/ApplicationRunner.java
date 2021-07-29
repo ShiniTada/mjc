9da48ce3d1ed6389f05f064f3ad1299bdd4f2a3e
Natalia <natallia_kuzmenka@epam.com>
2021-07-29 10:14:24 +0300

    commit

--- web/src/main/java/com/epam/esm/web/ApplicationRunner.java
+++ web/src/main/java/com/epam/esm/web/ApplicationRunner.java
@@ -3,12 +3,13 @@ package com.epam.esm.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * ApplicationRunner
  */
 @SpringBootApplication
-public class ApplicationRunner {
+public class ApplicationRunner extends SpringBootServletInitializer {
 
     /**
      * The entry point of application.
